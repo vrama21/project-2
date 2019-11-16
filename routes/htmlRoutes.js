fdfff48f725dba2fe873540ddc21b02d2c1a0f69
Valon Rama <vrama21@gmail.com>
2019-11-16 12:08:49 -0500

    Updated routes and some minor refactoring across all files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,31 @@
-var db = require("../models");
+const db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+module.exports = (app) => {
+    // Load index page
+    app.get("/", (req, res) => {
+        db.Inventory.findAll({}).then((inventory) => {
+            res.render("index", {
+                msg: "Welcome!",
+                inventory: inventory
+            });
+        });
     });
-  });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+    // Load example page and pass in an example by id
+    app.get("/inventory/:id", (req, res) => {
+        db.Example.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then((inventory_item) => {
+            res.render("example", {
+                inventory: inventory_item
+            });
+        });
     });
-  });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+    // Render 404 page for any unmatched routes
+    app.get("*", (req, res) => {
+        res.render("404");
+    });
 };
