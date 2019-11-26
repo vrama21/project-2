03bd435bee6c378de09fbb1bccb3140e0e248438
ErikBoerma <eboerma@gmail.com>
2019-11-26 18:39:53 -0500

    added order.js

--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,4 @@ CREATE DATABASE pos_db;
 
 -- https://cdn2.iconfinder.com/data/icons/meat-18/50/19-512.png :: Steak
 -- https://image.flaticon.com/icons/svg/112/112291.svg :: Chicken
+-- https://cdn3.iconfinder.com/data/icons/holidays-15/34/32-512.png :: fish
