df07221bf9e45945fa3b582c7c6cd8171e8940e4
Valon Rama <vrama21@gmail.com>
2019-12-02 21:55:44 -0500

    Started work on submit order

--- public/js/ticketFrontEnd.js
+++ public/js/ticketFrontEnd.js
@@ -122,28 +122,37 @@ const submitOrder = () => {
     let newOrderArray = [];
 
     const tableRows = $("#ticket-table-body").children("tr");
+    // for (let i = 0; i < tableRows.length; i++) {
+    //     const tableRow = tableRows[i];
+    //     const productQuantityCell = $(tableRow).children()[1];
+    //     const productNameCell = $(tableRow).children()[2];
+    //     const priceCell = $(tableRow).children()[3];
+
+    //     const newOrder = {
+    //         productQuantity: $(productQuantityCell).text(),
+    //         productName: $(productNameCell).text(),
+    //         price: $(priceCell).text().substring(1)
+    //     };
+
+    //     newOrderArray.push(newOrder);
+    // };
+
     for (let i = 0; i < tableRows.length; i++) {
         const tableRow = tableRows[i];
-        const productQuantityCell = $(tableRow).children()[1];
-        const productNameCell = $(tableRow).children()[2];
-        const priceCell = $(tableRow).children()[3];
-
-        const newOrder = {
-            productQuantity: $(productQuantityCell).text(),
-            productName: $(productNameCell).text(),
-            price: $(priceCell).text().substring(1)
-        };
-
-        newOrderArray.push(newOrder);
-    };
+    }
 
     $.ajax({
-        method: "POST",
-        url: "/api/order",
-        data: newOrderArray[0]
-    }).then(() => {
-        location.replace("/");
-    });
+        method: "PUT",
+        url: `/api/inventory/${itemId}`
+    })
+
+    // $.ajax({
+    //     method: "POST",
+    //     url: "/api/order",
+    //     data: newOrderArray[0]
+    // }).then(() => {
+    //     location.replace("/");
+    // });
 };
 
 const increaseQuantity = (quantityCell, priceCell, netPriceCell) => {
